a176b01459e5c694e549bd3ff5c2f36de22e52d0
Maciej Rybarczyk <maciej.rybarczyk@onet.pl>
2024-12-07 15:36:45 +0100

    Database setup and basic configuration
    Added .env file
    Added podman compose file to run db in a container
    Added temporary test route to test db connection
    Updated requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'chudeszczury1'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    db.init_app(app)
 
     from .routes import routes
     from .auth import auth
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
 from flask import Blueprint
-
+from app import db
+from sqlalchemy import text
 routes = Blueprint('routes', __name__)
 
 
 @routes.route('/')
 def home():
-    return '<h1>Welcome to Gym Management 1.0</h1>'
+    # return '<h1>Welcome to Gym Management 1.0</h1>'
+    try:
+        # Test the database connection with a simple query
+        result = db.session.execute(text('SELECT 1'))
+        return "Database connection successful!", 200
+    except Exception as e:
+        return f"Error: {e}", 500
