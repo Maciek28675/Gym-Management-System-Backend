742727088a7f38ec529eadb7d82d5eca69af204a
Maciej Rybarczyk <maciej.rybarczyk@onet.pl>
2024-12-12 11:14:38 +0100

    Add register route
    register route is not ready yet - requires modification

--- app/auth.py
+++ app/auth.py
@@ -7,6 +7,26 @@ from .models import Employee
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/api/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    employee_id = data['employee_id']
+    password = data['password']
+    gym_id = data['gym_id']
+    first_name = data['first_name']
+    last_name = data['last_name']
+    role = data['role']
+
+    user = Employee.query.filter_by(employee_id=employee_id).first()
+
+    if user:
+        return jsonify({"msg": "User already exists"}), 400
+
+    hashed_password = Bcrypt.generate_password_hash(password).decode('utf-8')
+
+    return jsonify({"msg": "User registered successfully"}), 201
+
 @auth.route('/api/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -17,6 +37,8 @@ def login():
 
     user = Employee.query.filter_by(employee_id=employee_id).first()
 
+    print(f'user: {user}')
+    
     if user and Bcrypt.check_password_hash(user.password, password):
         access_token = create_access_token(identity=user.employee_id)
 
