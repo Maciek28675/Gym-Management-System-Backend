6c20f9afe45c485a260a32924cd03e493918e67a
Patryk Piwnicki <272572@student.pwr.edu.pl>
2024-12-19 14:36:03 +0100

    Add gym_id choice in logging in

--- app/auth.py
+++ app/auth.py
@@ -17,20 +17,24 @@ def first_register():
     manager_exists = Employee.query.filter_by(role='manager').first()
 
     if manager_exists:
+        logging.error("Attempt to access 'first_register' route when a manager already exists.")
         return jsonify({'msg': "A manager already exists. This route is no longer accessible."}), 403
     
     data = request.get_json()
 
     if not data:
+        logging.error("No data provided for 'first_register'")
         return jsonify({"msg": "No data provided"}), 400
 
     required_fields = {'password', 'gym_id', 'first_name', 'last_name', 'role'}
     
     for field in required_fields:
         if field not in data:
+            logging.error(f"Missing required field in 'first_register': {field}")
             return jsonify({"msg": f"Field '{field}' is required"}), 400
 
     if data['role'].lower() != 'manager':
+        logging.error("Invalid role provided for 'first_register'. First user must be a manager.")
         return jsonify({"msg": "The first user must have the role 'manager'"}), 400
 
 
@@ -49,6 +53,7 @@ def first_register():
 
         db.session.add(new_employee)
         db.session.commit()
+        logging.info(f"Manager registered successfully: {data['first_name']} {data['last_name']}, Gym ID: {data['gym_id']}")
         return jsonify({"msg": "User registered successfully"}), 201
 
     except Exception as e:
@@ -63,12 +68,14 @@ def register():
     data = request.get_json()
 
     if not data:
+        logging.error("No data provided for 'register'")
         return jsonify({"msg": "No data provided"}), 400
 
     required_fields = {'password', 'gym_id', 'first_name', 'last_name', 'role'}
 
     for field in required_fields:
         if field not in data:
+            logging.error(f"Missing required field in 'register': {field}")
             return jsonify({"msg": f"Field '{field}' is required"}), 400
 
     try:
@@ -86,7 +93,7 @@ def register():
 
         db.session.add(new_employee)
         db.session.commit()
-
+        logging.info(f"Employee registered successfully: {data['first_name']} {data['last_name']}, Role: {data['role']}, Gym ID: {data['gym_id']}")
         return jsonify({"msg": "User registered successfully"}), 201
 
     except Exception as e:
@@ -101,32 +108,39 @@ def login():
     data = request.get_json()
 
     if not data:
+        logging.error("No data provided for login")
         return jsonify({"msg": "No data provided"}), 400
 
-    required_fields = {'employee_id', 'password'}
+    required_fields = {'employee_id', 'password', 'gym_id'}
 
     for field in required_fields:
         if field not in data:
+            logging.error(f"Missing required field: {field}")
             return jsonify({"msg": f"Field '{field}' is required"}), 400
 
     employee_id = data['employee_id']
     password = data['password']
+    gym_id = data['gym_id']
 
     try:
-        user = Employee.query.filter_by(employee_id=employee_id).first()
+        user = Employee.query.filter_by(employee_id=employee_id, gym_id=gym_id).first()
         if user:
             bytes_password = password.encode('utf-8')
             user_bytes = user.password.encode('utf-8')
 
             if bcrypt.checkpw(bytes_password, user_bytes):
-                access_token = create_access_token(identity=str(user.employee_id), additional_claims={"role": user.role})
+                access_token = create_access_token(
+                    identity=str(user.employee_id),
+                    additional_claims={"role": user.role, "gym_id": gym_id}
+                )
+                logging.info(f"User {employee_id} logged in successfully at gym {gym_id}")
                 return jsonify({'message': 'Login Success', 'access_token': access_token}), 200
             else:
-                logging.warning(f"Failed login attempt for employee_id: {employee_id}")
+                logging.warning(f"Failed login attempt for employee_id: {employee_id} at gym {gym_id}")
                 return jsonify({'message': 'Invalid credentials'}), 401
 
-        logging.warning(f"Login attempt for non-existing user: {employee_id}")
-        return jsonify({'message': 'User does not exist'}), 404
+        logging.warning(f"Login attempt for non-existing user or invalid gym: employee_id {employee_id}, gym_id {gym_id}")
+        return jsonify({'message': 'User does not exist or gym mismatch'}), 404
 
     except Exception as e:
         logging.error(f"An error occurred during login: {str(e)}")
